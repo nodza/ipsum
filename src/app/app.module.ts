2cc05e3dfa71a336bc795d6d99199ff240b34695
nodza <noel.hwande@gmail.com>
2017-10-24 20:48:29 -0500

    Added styles for Detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { PostService } from './post.service';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { LengthPipe } from './length.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    DetailsComponent
+    DetailsComponent,
+    LengthPipe
   ],
   imports: [
     BrowserModule,
